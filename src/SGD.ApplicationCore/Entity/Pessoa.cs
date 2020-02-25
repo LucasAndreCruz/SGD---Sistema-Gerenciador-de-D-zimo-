a6f5de95121904cf7d59f6ba086c63e6a7031445
LUCAS ANDRÉ <lucas_andre_spfc@hotmail.com>
2020-02-25 03:18:44 -0300

    Criando camada do projeto e modelo de classe Pessoa part 1

---
+++ src/SGD.ApplicationCore/Entity/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGD.ApplicationCore.Entity
+{
+   public class Pessoa
+    {
+
+        public Pessoa()
+        {
+
+        }
+
+
+        public int PessoaId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public string RG { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
